4d59bcd539338b3c9ed117a01a5c28b3c7147fb8
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2025-06-23 13:35:06 +0200

    maintenance removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import {MaintenanceComponent} from "./components/maintenance/maintenance.component";
+import {HomeComponent} from './components/home/home.component';
+import {StaffPageComponent} from './components/staffs/staff-page/staff-page.component';
 
 export const routes: Routes = [
-  { path: '', component: MaintenanceComponent }
+  { path: '', component: HomeComponent },
+  { path: 'staff/:id', component: StaffPageComponent }
 ];
