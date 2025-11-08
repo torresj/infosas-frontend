2ab6e7b64f1d97ad7d0b1506837d8f13bb926544
jaime <jtbenavente@gmail.com>
2025-11-08 14:00:22 +0100

    start maintenance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {StaffPageComponent} from './components/staffs/staff-page/staff-page.component';
+import {MaintenanceComponent} from "./components/maintenance/maintenance.component";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'staff/:id', component: StaffPageComponent }
+  { path: '', component: MaintenanceComponent }
 ];
