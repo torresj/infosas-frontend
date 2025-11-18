6b4c2cbd4bbd6e4fc6199d969b2f8046f8b53a45
jaime <jtbenavente@gmail.com>
2025-11-18 14:57:05 +0100

    new types added

--- src/app/components/staffs/staff/staff.component.ts
+++ src/app/components/staffs/staff/staff.component.ts
@@ -22,19 +22,52 @@ export class StaffComponent {
 
   getStaffType(): string {
     switch (this.staff.type){
-      case StaffType.FISIO: return "Fisioterapeuta";
-      case StaffType.TCAE: return "Técnico/a en Cuidados Auxiliares de Enfermería";
-      case StaffType.OCCUPATIONAL_THERAPY: return "Terapeuta ocupacional";
-      case StaffType.NURSE: return "Enfermero/a";
-      case StaffType.SPEECH_THERAPIST: return "Logopeda";
-      case StaffType.TEAP: return "Técnico/a Especialista en Anatomía Patológica";
-      case StaffType.TEDN: return "Técnico/a Especialista Dietética y Nutrición";
-      case StaffType.TEDS: return "Técnico/a Especialista en Documentación Sanitaria";
-      case StaffType.TEL: return "Técnico/a Especialista en Laboratorio";
-      case StaffType.TEMN: return "Técnico/a Especialista en Medicina Nuclear";
-      case StaffType.TER: return "Técnico/a Especialista en Radioterapia";
-      case StaffType.TERD: return "Técnico/a Especialista en Radiodiagnóstico";
-      case StaffType.TF: return "Técnico/a en Farmacia";
+        case StaffType.FISIO: return "Fisioterapeuta";
+        case StaffType.TCAE: return "Técnico/a en Cuidados Auxiliares de Enfermería";
+        case StaffType.OCCUPATIONAL_THERAPY: return "Terapeuta ocupacional";
+        case StaffType.NURSE: return "Enfermero/a";
+        case StaffType.SPEECH_THERAPIST: return "Logopeda";
+        case StaffType.TEAP: return "Técnico/a Especialista en Anatomía Patológica";
+        case StaffType.TEDN: return "Técnico/a Especialista Dietética y Nutrición";
+        case StaffType.TEDS: return "Técnico/a Especialista en Documentación Sanitaria";
+        case StaffType.TEL: return "Técnico/a Especialista en Laboratorio";
+        case StaffType.TEMN: return "Técnico/a Especialista en Medicina Nuclear";
+        case StaffType.TER: return "Técnico/a Especialista en Radioterapia";
+        case StaffType.TERD: return "Técnico/a Especialista en Radiodiagnóstico";
+        case StaffType.TF: return "Técnico/a en Farmacia";
+        case StaffType.PODIATRIST: return "Podólogo/a";
+        case StaffType.TSESPC: return  "Téc. Salud Educación para la Salud y Participación Comunitaria";
+        case StaffType.TSSA: return "Téc. Salud Sanidad Ambiental";
+        case StaffType.TSPRLPA: return "Técnico/a Superior en Prevención de Riesgos Laborales-Ergonomía y Psicosociología Aplicada";
+        case StaffType.TSPRLHI: return "Técnico/a Superior en Prevención de Riesgos Laborales-Higiene Industrial";
+        case StaffType.TSPRLST: return "Técnico/a Superior en Prevención de Riesgos Laborales-Seguridad en el Trabajo";
+        case StaffType.SOCIAL_WORKER: return "Trabajador/a Social";
+        case StaffType.ADM: return "Administrativo/a";
+        case StaffType.ADMINISTRATIVE_ASSISTANT: return "Auxiliar Administrativo/a";
+        case StaffType.CC: return "Celador/a Conductor/a";
+        case StaffType.CCUL: return "Celador/a-Conductor/a de Unidad Logística";
+        case StaffType.CCCTS: return "Celador/a-Conductor/a C.T.S.";
+        case StaffType.CCAA: return "Celador/a-Conductor/a de ambulancia asistencial tipo B y C";
+        case StaffType.COOK: return "Cocinero/a";
+        case StaffType.POC: return "Personal de Oficio Costurero/a";
+        case StaffType.MONITOR: return "Monitor/a";
+        case StaffType.POP: return "Personal de oficio Peluquero/a";
+        case StaffType.TEI: return "Técnico/a Especialista en Informática";
+        case StaffType.TSA: return "Técnico/a Superior en Alojamiento";
+        case StaffType.PHONE: return "Telefonista";
+        case StaffType.TEMEII: return "Técnico/a Especialista en Mantenimiento de Edificios e Instalaciones Industriales";
+        case StaffType.TEMEITF: return "T. E. M. E. I para Área de Instalaciones Térmicas y de Fluidos";
+        case StaffType.TEMEISEA: return "T. E. M. E. I. para Área de Sistemas Electrotécnicos y Automatizados";
+        case StaffType.TMAC: return "Técnico/a de Mantenimiento Acabados de Construcción";
+        case StaffType.TMMM: return "Técnico/a de Mantenimiento de Madera y Mueble";
+        case StaffType.TMOA: return "Técnico/a de Mantenimiento Obras de Albañilería";
+        case StaffType.TEE: return "Técnico/a Especialista en Electromedicina";
+        case StaffType.TIPRL: return "Técnico/a Intermedio/a en Prevención de Riesgos Laborales";
+        case StaffType.WARDEN: return "Celador/a";
+        case StaffType.CLEANER: return "Limpiador/a";
+        case StaffType.LAUNDRY_STAFF: return "Personal Lavandería - Planchado";
+        case StaffType.LABOURER: return "Peón";
+        case StaffType.HELPER: return "Pinche";
     }
   }
 
